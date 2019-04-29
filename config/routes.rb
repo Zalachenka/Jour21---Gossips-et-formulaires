39a48b8123b59f5df59b07b20fb6530a5f1f77fb
tsubakiandco <ayuma.royet@gmail.com>
2019-04-29 15:29:49 +0200

    @nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home', to: 'home#home'
+  get '/team', to: 'team#team'
+  get 'contact/contact'
 end
