d395fe592b6f07422fbd8a42cbf061cadbd20106
tsubakiandco <ayuma.royet@gmail.com>
2019-04-29 18:57:51 +0200

    Gossip pages

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -1,4 +1,5 @@
 class GossipController < ApplicationController
   def gossip
+  @gossip = Gossip.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'gossip/gossip'
+
   get 'welcome/:user_entry', to: 'welcome#first_name'
   get '/home', to: 'home#home'
   get '/team', to: 'team#team'
   get '/contact', to: 'contact#contact'
+  get '/gossip/:id', to: 'gossip#gossip'
 end
